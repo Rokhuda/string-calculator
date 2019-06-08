b159126c40d9af484df55b2f95562d9bd95db99f
rokhuda <rokhuda@gmail.com>
2019-06-08 13:17:23 +0200

    cleaned redundant code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,27 +5,13 @@ function add(stringNumbers) {
     if (stringNumbers == '') return 0;
 
     let sum = 0;
-    let newArray = stringNumbers.split(/^(?=.*\r)(?=.*\n)$/);
+    let newArray = stringNumbers.split(/\D/g);
 
-
-    console.log(newArray)
-    console.log(typeof newArray)
 
     for (let i = 0; i < newArray.length; i++) {
-        if (newArray[i] != '' && typeof newArray[i] == Number) {
-            if (parseInt(newArray[i]) > 0) {
-                sum += parseInt(newArray[i]);
-                console.log(newArray[i])
-            } else {
-                sum = 'negative number (-1) not allowed';
-                return sum;
-            }
+        if (newArray[i] != '' && isNaN(newArray)) {
+            sum += parseInt(newArray[i]);
         }
-
     }
     return sum;
 }
-//console.log(sum)
-
-
-console.log(add('//;\n1;2')) 
