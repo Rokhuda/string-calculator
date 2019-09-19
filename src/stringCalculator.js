d8bcd59b7fe5536b4f99d256015a2fb0ba0a2119
rokhuda <rokhuda@gmail.com>
2019-09-19 17:48:28 +0200

    modified code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,13 +5,18 @@ function add(stringNumbers) {
     if (stringNumbers == '') return 0;
 
     let sum = 0;
-    let newArray = stringNumbers.split(/\D/g);
 
+    let newArray = stringNumbers.match(/-?\d+/g)
 
     for (let i = 0; i < newArray.length; i++) {
-        if (newArray[i] != '' && isNaN(newArray)) {
+        if (newArray[i] > 0) {
             sum += parseInt(newArray[i]);
+        } else {
+            return ('negatives not allowed' + ' and the negative number is: ' + newArray[i]);
         }
+
     }
     return sum;
 }
+
+console.log(add('//;\n12;;33;;;;-2'))
